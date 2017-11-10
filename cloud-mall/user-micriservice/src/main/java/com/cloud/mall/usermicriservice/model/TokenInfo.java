db6f945de0e7e77544f98381be948dcf609a28ba
Jon_China <503665965@qq.com>
2017-11-10 23:52:46 +0800

    feat(token):token实体
    token实体

---
+++ cloud-mall/user-micriservice/src/main/java/com/cloud/mall/usermicriservice/model/TokenInfo.java
@@ -0,0 +1,40 @@
+package com.cloud.mall.usermicriservice.model;
+
+import java.io.Serializable;
+
+/**
+ * token
+ */
+public class TokenInfo implements Serializable {
+    private static final long serialVersionUID = 8252838869078177957L;
+
+    private String userId;
+
+    private Long expires;
+
+    private String appName;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Long getExpires() {
+        return expires;
+    }
+
+    public void setExpires(Long expires) {
+        this.expires = expires;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+}
